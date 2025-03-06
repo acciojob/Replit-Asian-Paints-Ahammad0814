ccdcd18d9151585fe57da22d5e4810b823f93e9c
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-03-06 20:25:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const gridContainer = document.getElementById('grid-container');
+    const blockIdInput = document.getElementById('block_id');
+    const colorInput = document.getElementById('colour_id');
+    const changeButton = document.getElementById('change_button');
+    const resetButton = document.getElementById('Reset');
+
+    changeButton.addEventListener('click', () => {
+      const blockId = blockIdInput.value;
+      const color = colorInput.value;
+
+        const gridItems = document.querySelectorAll('.grid-item');
+        gridItems.forEach(item => {
+            item.style.backgroundColor = 'transparent';
+        });
+
+      const gridItem = document.getElementById(blockId);
+      if (gridItem) {
+        gridItem.style.backgroundColor = color;
+      } else {
+        alert('Invalid Block ID');
+      }
+    });
+
+    resetButton.addEventListener('click', () => {
+      const gridItems = document.querySelectorAll('.grid-item');
+      gridItems.forEach(item => {
+          item.style.backgroundColor = 'transparent';
+      });
+      blockIdInput.value = '';
+      colorInput.value = '';
+    });
